e82ac752b2c9b96cf4e21a8ea890d89d8033ea5f
AyaMohamed <ayamohamedmahmoud@gmail.com>
2025-07-29 23:41:35 +0300

    Add TestRunner class to execute features

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,14 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions", "hooks"},
+        plugin = {"pretty", "html:target/cucumber-report.html"},
+        tags = "@scroll and @apidemos"
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
